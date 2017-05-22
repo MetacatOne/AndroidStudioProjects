0fbe8d81df258d5f1ba3c5822e6b20d52c918d06
MetacatOne <barzu.george@gmail.com>
2017-05-22 21:33:40 +0300

    Flickr App added

---
+++ FlickrBrowser/app/src/main/java/com/metacat/flickrbrowser/GetFlickJsonData.java
@@ -0,0 +1,13 @@
+package com.metacat.flickrbrowser;
+
+/**
+ * Created by gbarzu on 22.05.2017.
+ */
+
+public class GetFlickJsonData extends GetRawData {
+
+    public GetFlickJsonData(String searchCriteria, boolean matchAll) {
+        super(null);
+
+    }
+}
